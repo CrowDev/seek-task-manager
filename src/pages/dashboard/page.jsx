d92c0707d21e9204a82d71c6b2ea0428dc1f0efe
Hardy Aguilar <h.aguilarvalenzuela@gmail.com>
2025-04-09 16:02:49 -0400

    feat: fetch data from backend

--- src/pages/dashboard/page.jsx
+++ src/pages/dashboard/page.jsx
@@ -1,5 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 const DashboardPage = () => {
-  return <h1>Dashboard</h1>;
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      setTasks(data);
+    };
+    fetchTasks();
+  }, []);
+
+  return tasks.map((task) => {
+    return (
+      <div key={task._id} className="text-gray-700">
+        <h2>{task.title}</h2>
+        <p>{task.description}</p>
+        <p>{task.dueDate}</p>
+        <p>{task.state}</p>
+      </div>
+    );
+  });
 };
 
 export default DashboardPage;
