a1980ccedc71beb6a272ab3e82daf4d22733e7e1
Hardy Aguilar <h.aguilarvalenzuela@gmail.com>
2025-04-09 19:42:48 -0400

    feat: add dialog to create tasks

---
+++ src/components/dialog/TaskFormDialog.jsx
@@ -0,0 +1,125 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Grid from "@mui/material/Grid";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Divider from "@mui/material/Divider";
+import { useState } from "react";
+
+const TaskFormDialog = ({ open, handleClose }) => {
+  const [priority, setPriority] = useState("");
+  const [status, setStatus] = useState("");
+
+  const onChangePriority = (event) => {
+    setPriority(event.target.value);
+  };
+
+  const onChangeStatus = (event) => {
+    setStatus(event.target.value);
+  };
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        maxWidth="sm"
+        fullWidth={true}
+        slotProps={{
+          paper: {
+            component: "form",
+            onSubmit: (event) => {
+              event.preventDefault();
+              const formData = new FormData(event.currentTarget);
+              const formJson = Object.fromEntries(formData.entries());
+              console.log(formJson);
+              // handleClose();
+            },
+          },
+        }}
+      >
+        <DialogTitle>Create new task</DialogTitle>
+        <Divider />
+        <DialogContent>
+          <div className="mb-3">
+            <TextField
+              autoFocus
+              required
+              margin="dense"
+              id="title"
+              name="title"
+              label="Title"
+              type="text"
+              fullWidth
+              variant="outlined"
+            />
+          </div>
+          <div className="mb-3">
+            <TextField
+              required
+              margin="dense"
+              id="description"
+              name="description"
+              label="Description"
+              type="text"
+              fullWidth
+              variant="outlined"
+              multiline
+              rows={3}
+            />
+          </div>
+          <Grid container spacing={3} columns={2}>
+            <Grid size="grow">
+              <FormControl fullWidth>
+                <InputLabel id="priority-select-label">Priority</InputLabel>
+                <Select
+                  labelId="priority-select-label"
+                  id="priority-select"
+                  name="priority"
+                  label="Priority"
+                  value={priority}
+                  onChange={onChangePriority}
+                >
+                  <MenuItem value={"High"}>High</MenuItem>
+                  <MenuItem value={"Medium"}>Medium</MenuItem>
+                  <MenuItem value={"Low"}>Low</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid size="grow">
+              <FormControl fullWidth>
+                <InputLabel id="status-select-label">Status</InputLabel>
+                <Select
+                  labelId="status-select-label"
+                  id="status-select"
+                  name="status"
+                  label="Status"
+                  vale={status}
+                  onChange={onChangeStatus}
+                >
+                  <MenuItem value={"todo"}>Todo</MenuItem>
+                  <MenuItem value={"in-progress"}>In Progress</MenuItem>
+                  <MenuItem value={"in-review"}>In Review</MenuItem>
+                  <MenuItem value={"done"}>Done</MenuItem>
+                  <MenuItem value={"blocked"}>Blocked</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button type="submit">Create Task</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default TaskFormDialog;
--- src/pages/dashboard/page.jsx
+++ src/pages/dashboard/page.jsx
@@ -4,11 +4,13 @@ import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Chip from "@mui/material/Chip";
+import TaskFormDialog from "@/components/dialog/TaskFormDialog";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 const DashboardPage = () => {
   const [tasks, setTasks] = useState([]);
+  const [openDialogTask, setOpenDialogTask] = useState(false);
 
   useEffect(() => {
     const fetchTasks = async () => {
@@ -18,6 +20,14 @@ const DashboardPage = () => {
     };
     fetchTasks();
   }, []);
+
+  const handleOpenDialogTask = () => {
+    setOpenDialogTask(true);
+  };
+
+  const handleCloseDialogTask = () => {
+    setOpenDialogTask(false);
+  };
 
   const getPriorityColor = (priority) => {
     const colors = {
@@ -39,199 +49,223 @@ const DashboardPage = () => {
   const blockedTasks = tasks.filter((task) => task.status === "blocked");
 
   return (
-    <section className="mx-auto max-w-7xl">
-      <article className="mb-3">
-        <h1 className="text-2xl font-semibold text-primary-blue">
-          Dev Team Task Board
-        </h1>
-      </article>
-      <article className="flex py-2 mb-3 space-x-4 border-b border-secondary-gray">
-        <button className="py-2 px-3 font-semibold rounded-lg transition-colors hover:cursor-pointer text-secondary-gray hover:bg-secondary-silver">
-          Board
-        </button>
-        <button className="py-2 px-3 font-semibold rounded-lg transition-colors hover:cursor-pointer text-secondary-gray hover:bg-secondary-silver">
-          List
-        </button>
-      </article>
+    <>
+      <section className="mx-auto max-w-7xl">
+        <article className="mb-3">
+          <h1 className="text-2xl font-semibold text-primary-blue">
+            Dev Team Task Board
+          </h1>
+        </article>
+        <article className="flex justify-between py-2 mb-3 border-b border-secondary-gray">
+          <div className="flex space-x-4">
+            <button className="py-2 px-3 font-semibold rounded-lg transition-colors hover:cursor-pointer text-secondary-gray hover:bg-secondary-silver">
+              Board
+            </button>
+            <button className="py-2 px-3 font-semibold rounded-lg transition-colors hover:cursor-pointer text-secondary-gray hover:bg-secondary-silver">
+              List
+            </button>
+          </div>
+          <button
+            onClick={handleOpenDialogTask}
+            className="py-2 px-3 font-semibold rounded-lg transition-colors hover:cursor-pointer text-secondary-gray hover:bg-secondary-silver"
+          >
+            Create new task
+          </button>
+        </article>
 
-      <Grid container spacing={3} columns={5}>
-        <Grid size="grow">
-          <Box>
-            <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
-              <div className="py-2 px-3 mb-3 text-lg font-semibold bg-secondary-gray text-neutral-mist">
-                <span>To Do</span>
-              </div>
-              <div className="flex flex-col py-2 px-3 space-y-5">
-                {todoTasks.map((task) => (
-                  <Paper
-                    key={task._id}
-                    sx={{
-                      borderLeft: "5px solid",
-                      borderColor: getPriorityColor(task.priority),
-                      padding: "10px",
-                    }}
-                  >
-                    <div className="flex mb-3">
-                      <span className="font-semibold text-neutral-charcoal">
-                        {task.title}
-                      </span>
-                      <Chip
-                        sx={{
-                          backgroundColor: getPriorityColor(task.priority),
-                          color: "#f8f9fa",
-                        }}
-                        label={task.priority.toUpperCase()}
-                      />
-                    </div>
-                    <p className="text-neutral-charcoal">{task.description}</p>
-                  </Paper>
-                ))}
-              </div>
-            </Paper>
-          </Box>
+        <Grid container spacing={3} columns={5}>
+          <Grid size="grow">
+            <Box>
+              <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
+                <div className="py-2 px-3 mb-3 text-lg font-semibold bg-secondary-gray text-neutral-mist">
+                  <span>To Do</span>
+                </div>
+                <div className="flex flex-col py-2 px-3 space-y-5">
+                  {todoTasks.map((task) => (
+                    <Paper
+                      key={task._id}
+                      sx={{
+                        borderLeft: "5px solid",
+                        borderColor: getPriorityColor(task.priority),
+                        padding: "10px",
+                      }}
+                    >
+                      <div className="flex mb-3">
+                        <span className="font-semibold text-neutral-charcoal">
+                          {task.title}
+                        </span>
+                        <Chip
+                          sx={{
+                            backgroundColor: getPriorityColor(task.priority),
+                            color: "#f8f9fa",
+                          }}
+                          label={task.priority.toUpperCase()}
+                        />
+                      </div>
+                      <p className="text-neutral-charcoal">
+                        {task.description}
+                      </p>
+                    </Paper>
+                  ))}
+                </div>
+              </Paper>
+            </Box>
+          </Grid>
+          <Grid size="grow">
+            <Box>
+              <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
+                <div className="py-2 px-3 mb-3 text-lg font-semibold bg-primary-teal text-neutral-mist">
+                  <span>In Progress</span>
+                </div>
+                <div className="flex flex-col py-2 px-3 space-y-5">
+                  {inProgressTasks.map((task) => (
+                    <Paper
+                      key={task._id}
+                      sx={{
+                        borderLeft: "5px solid",
+                        borderColor: getPriorityColor(task.priority),
+                        padding: "10px",
+                      }}
+                    >
+                      <div className="flex mb-3">
+                        <span className="font-semibold text-neutral-charcoal">
+                          {task.title}
+                        </span>
+                        <Chip
+                          sx={{
+                            backgroundColor: getPriorityColor(task.priority),
+                            color: "#f8f9fa",
+                          }}
+                          label={task.priority.toUpperCase()}
+                        />
+                      </div>
+                      <p className="text-neutral-charcoal">
+                        {task.description}
+                      </p>
+                    </Paper>
+                  ))}
+                </div>
+              </Paper>
+            </Box>
+          </Grid>
+          <Grid size="grow">
+            <Box>
+              <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
+                <div className="py-2 px-3 mb-3 text-lg font-semibold bg-accent-amber text-neutral-mist">
+                  <span>In Review</span>
+                </div>
+                <div className="flex flex-col py-2 px-3 space-y-5">
+                  {inReviewTasks.map((task) => (
+                    <Paper
+                      key={task._id}
+                      sx={{
+                        borderLeft: "5px solid",
+                        borderColor: getPriorityColor(task.priority),
+                        padding: "10px",
+                      }}
+                    >
+                      <div className="flex mb-3">
+                        <span className="font-semibold text-neutral-charcoal">
+                          {task.title}
+                        </span>
+                        <Chip
+                          sx={{
+                            backgroundColor: getPriorityColor(task.priority),
+                            color: "#f8f9fa",
+                          }}
+                          label={task.priority.toUpperCase()}
+                        />
+                      </div>
+                      <p className="text-neutral-charcoal">
+                        {task.description}
+                      </p>
+                    </Paper>
+                  ))}
+                </div>
+              </Paper>
+            </Box>
+          </Grid>
+          <Grid size="grow">
+            <Box>
+              <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
+                <div className="py-2 px-3 mb-3 text-lg font-semibold bg-accent-success text-neutral-mist">
+                  <span>Done</span>
+                </div>
+                <div className="flex flex-col py-2 px-3 space-y-5">
+                  {doneTasks.map((task) => (
+                    <Paper
+                      key={task._id}
+                      sx={{
+                        borderLeft: "5px solid",
+                        borderColor: getPriorityColor(task.priority),
+                        padding: "10px",
+                      }}
+                    >
+                      <div className="flex mb-3">
+                        <span className="font-semibold text-neutral-charcoal">
+                          {task.title}
+                        </span>
+                        <Chip
+                          sx={{
+                            backgroundColor: getPriorityColor(task.priority),
+                            color: "#f8f9fa",
+                          }}
+                          label={task.priority.toUpperCase()}
+                        />
+                      </div>
+                      <p className="text-neutral-charcoal">
+                        {task.description}
+                      </p>
+                    </Paper>
+                  ))}
+                </div>
+              </Paper>
+            </Box>
+          </Grid>
+          <Grid size="grow">
+            <Box>
+              <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
+                <div className="py-2 px-3 mb-3 text-lg font-semibold bg-accent-coral text-neutral-mist">
+                  <span>Blocked</span>
+                </div>
+                <div className="flex flex-col py-2 px-3 space-y-5">
+                  {blockedTasks.map((task) => (
+                    <Paper
+                      key={task._id}
+                      sx={{
+                        borderLeft: "5px solid",
+                        borderColor: getPriorityColor(task.priority),
+                        padding: "10px",
+                      }}
+                    >
+                      <div className="flex mb-3">
+                        <span className="font-semibold text-neutral-charcoal">
+                          {task.title}
+                        </span>
+                        <Chip
+                          sx={{
+                            backgroundColor: getPriorityColor(task.priority),
+                            color: "#f8f9fa",
+                          }}
+                          label={task.priority.toUpperCase()}
+                        />
+                      </div>
+                      <p className="text-neutral-charcoal">
+                        {task.description}
+                      </p>
+                    </Paper>
+                  ))}
+                </div>
+              </Paper>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid size="grow">
-          <Box>
-            <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
-              <div className="py-2 px-3 mb-3 text-lg font-semibold bg-primary-teal text-neutral-mist">
-                <span>In Progress</span>
-              </div>
-              <div className="flex flex-col py-2 px-3 space-y-5">
-                {inProgressTasks.map((task) => (
-                  <Paper
-                    key={task._id}
-                    sx={{
-                      borderLeft: "5px solid",
-                      borderColor: getPriorityColor(task.priority),
-                      padding: "10px",
-                    }}
-                  >
-                    <div className="flex mb-3">
-                      <span className="font-semibold text-neutral-charcoal">
-                        {task.title}
-                      </span>
-                      <Chip
-                        sx={{
-                          backgroundColor: getPriorityColor(task.priority),
-                          color: "#f8f9fa",
-                        }}
-                        label={task.priority.toUpperCase()}
-                      />
-                    </div>
-                    <p className="text-neutral-charcoal">{task.description}</p>
-                  </Paper>
-                ))}
-              </div>
-            </Paper>
-          </Box>
-        </Grid>
-        <Grid size="grow">
-          <Box>
-            <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
-              <div className="py-2 px-3 mb-3 text-lg font-semibold bg-accent-amber text-neutral-mist">
-                <span>In Review</span>
-              </div>
-              <div className="flex flex-col py-2 px-3 space-y-5">
-                {inReviewTasks.map((task) => (
-                  <Paper
-                    key={task._id}
-                    sx={{
-                      borderLeft: "5px solid",
-                      borderColor: getPriorityColor(task.priority),
-                      padding: "10px",
-                    }}
-                  >
-                    <div className="flex mb-3">
-                      <span className="font-semibold text-neutral-charcoal">
-                        {task.title}
-                      </span>
-                      <Chip
-                        sx={{
-                          backgroundColor: getPriorityColor(task.priority),
-                          color: "#f8f9fa",
-                        }}
-                        label={task.priority.toUpperCase()}
-                      />
-                    </div>
-                    <p className="text-neutral-charcoal">{task.description}</p>
-                  </Paper>
-                ))}
-              </div>
-            </Paper>
-          </Box>
-        </Grid>
-        <Grid size="grow">
-          <Box>
-            <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
-              <div className="py-2 px-3 mb-3 text-lg font-semibold bg-accent-success text-neutral-mist">
-                <span>Done</span>
-              </div>
-              <div className="flex flex-col py-2 px-3 space-y-5">
-                {doneTasks.map((task) => (
-                  <Paper
-                    key={task._id}
-                    sx={{
-                      borderLeft: "5px solid",
-                      borderColor: getPriorityColor(task.priority),
-                      padding: "10px",
-                    }}
-                  >
-                    <div className="flex mb-3">
-                      <span className="font-semibold text-neutral-charcoal">
-                        {task.title}
-                      </span>
-                      <Chip
-                        sx={{
-                          backgroundColor: getPriorityColor(task.priority),
-                          color: "#f8f9fa",
-                        }}
-                        label={task.priority.toUpperCase()}
-                      />
-                    </div>
-                    <p className="text-neutral-charcoal">{task.description}</p>
-                  </Paper>
-                ))}
-              </div>
-            </Paper>
-          </Box>
-        </Grid>
-        <Grid size="grow">
-          <Box>
-            <Paper sx={{ overflow: "hidden", backgroundColor: "#f8f9fa" }}>
-              <div className="py-2 px-3 mb-3 text-lg font-semibold bg-accent-coral text-neutral-mist">
-                <span>Blocked</span>
-              </div>
-              <div className="flex flex-col py-2 px-3 space-y-5">
-                {blockedTasks.map((task) => (
-                  <Paper
-                    key={task._id}
-                    sx={{
-                      borderLeft: "5px solid",
-                      borderColor: getPriorityColor(task.priority),
-                      padding: "10px",
-                    }}
-                  >
-                    <div className="flex mb-3">
-                      <span className="font-semibold text-neutral-charcoal">
-                        {task.title}
-                      </span>
-                      <Chip
-                        sx={{
-                          backgroundColor: getPriorityColor(task.priority),
-                          color: "#f8f9fa",
-                        }}
-                        label={task.priority.toUpperCase()}
-                      />
-                    </div>
-                    <p className="text-neutral-charcoal">{task.description}</p>
-                  </Paper>
-                ))}
-              </div>
-            </Paper>
-          </Box>
-        </Grid>
-      </Grid>
-    </section>
+      </section>
+      <TaskFormDialog
+        open={openDialogTask}
+        handleClose={handleCloseDialogTask}
+      />
+    </>
   );
 };
 
